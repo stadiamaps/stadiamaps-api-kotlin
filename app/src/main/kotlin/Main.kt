a154786b3446be5833e0a06fe7989f32358b0d97
Ian Wagner <ian.wagner@stadiamaps.com>
2023-05-17 04:03:51 +0900

    Demonstrate use of Geocoding APIs

--- app/src/main/kotlin/Main.kt
+++ app/src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import org.openapitools.client.apis.GeocodingApi
 import org.openapitools.client.apis.GeospatialApi
 import org.openapitools.client.apis.RoutingApi
 import org.openapitools.client.auth.ApiKeyAuth
@@ -8,27 +9,57 @@ const val apiKey = "YOUR-API-KEY"
 
 val seoul = Coordinate(37.56, 126.99)
 val tll = RoutingWaypoint(59.416411, 24.798778)
-val kultuurikatel = RoutingWaypoint(59.444169, 24.751779)
+val kultuurikatel = RoutingWaypoint(59.44436, 24.75071)
 
 fun testGeospatialApis(client: ApiClient) {
     val geoService = client.createService(GeospatialApi::class.java)
 
-    val tzRes = geoService.tzLookup(seoul.lat, seoul.lon).execute().body()
-    println(tzRes)
+    val tzRes = geoService.tzLookup(seoul.lat, seoul.lon).execute()
+    if (tzRes.isSuccessful) {
+        println(tzRes.body())
+    } else {
+        println("Request failed with error code ${tzRes.code()}")
+    }
 
-    val heightRes = geoService.elevationService(HeightRequest(shape = listOf(seoul))).execute().body()
-    println(heightRes)
+    val heightRes = geoService.elevationService(HeightRequest(shape = listOf(seoul))).execute()
+    if (heightRes.isSuccessful) {
+        println(heightRes.body())
+    } else {
+        println("Request failed with error code ${heightRes.code()}")
+    }
 }
 
 fun testRoutingApis(client: ApiClient) {
     val routingService = client.createService(RoutingApi::class.java)
 
     val waypoints = listOf(tll, kultuurikatel)
-    val route = routingService.route(RouteRequest(waypoints, CostingModel.auto)).execute().body()
+    val route = routingService.route(RouteRequest(waypoints, CostingModel.auto)).execute()
 
-    if (route != null) {
-        val summary = route.trip.summary
-        println("Found route with length of ${summary.length}km and duration of ${summary.time} seconds")
+    if (route.isSuccessful) {
+        val summary = route.body()?.trip?.summary
+        println("Found route with length of ${summary?.length}km and duration of ${summary?.time} seconds")
+    } else {
+        println("Request failed with error code ${route.code()}")
+    }
+}
+
+fun testGeocodingApis(client: ApiClient) {
+    val geocodingService = client.createService(GeocodingApi::class.java)
+
+    val structuredSearch = geocodingService.searchStructured(address = "11 Wall St", region = "New York", locality = "New York").execute()
+
+    if (structuredSearch.isSuccessful) {
+        println("Found structured search result: ${structuredSearch.body()?.features?.first()}")
+    } else {
+        println("Request failed with error code ${structuredSearch.code()}")
+    }
+
+    val reverseGeocode = geocodingService.reverse(kultuurikatel.lat, kultuurikatel.lon).execute()
+
+    if (reverseGeocode.isSuccessful) {
+        println("Found reverse geocode result: ${reverseGeocode.body()?.features?.first()}")
+    } else {
+        println("Request failed with error code ${reverseGeocode.code()}")
     }
 }
 
@@ -38,4 +69,5 @@ fun main(args: Array<String>) {
 
     testGeospatialApis(client)
     testRoutingApis(client)
+    testGeocodingApis(client)
 }
