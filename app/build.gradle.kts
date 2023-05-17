7fc5ab003675a8a918923ed801ea4f30529d262d
Ian Wagner <ian.wagner@stadiamaps.com>
2023-05-17 12:54:08 +0900

    Minor version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ dependencies {
 
     implementation(project(":generated-client"))
 
-    implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
-    implementation("com.squareup.moshi:moshi-adapters:1.13.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
+    implementation("com.squareup.moshi:moshi-adapters:1.14.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
     implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
     implementation("com.squareup.retrofit2:converter-moshi:$retrofitVersion")
