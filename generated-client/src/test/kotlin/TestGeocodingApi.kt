2a83bff9da92345f74db3046c240528eaf910dd5
Ian Wagner <ian.wagner@stadiamaps.com>
2023-06-27 00:24:35 +0900

    Add geocoding tests

---
+++ generated-client/src/test/kotlin/TestGeocodingApi.kt
@@ -0,0 +1,75 @@
+import com.stadiamaps.api.apis.GeocodingApi
+import com.stadiamaps.api.auth.ApiKeyAuth
+import com.stadiamaps.api.infrastructure.ApiClient
+import com.stadiamaps.api.infrastructure.CollectionFormats
+import com.stadiamaps.api.models.PeliasLayer
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.fail
+
+internal class TestGeocodingApi {
+    private val apiKey = System.getenv("STADIA_API_KEY") ?: throw RuntimeException("API Key not set")
+    private lateinit var service: GeocodingApi
+    private val address = "Põhja pst 27a"
+
+    @BeforeEach
+    fun setUp() {
+        val client = ApiClient()
+        client.addAuthorization("ApiKeyAuth", ApiKeyAuth("query", "api_key", apiKey))
+        service = client.createService(GeocodingApi::class.java)
+    }
+
+    @Test
+    fun testAutocomplete() {
+        val res = service.autocomplete(address).execute()
+        val body = res.body() ?: fail("Request failed: ${res.errorBody()}")
+
+        assertEquals("Estonia", body.features.first().properties!!.country)
+        assertEquals(PeliasLayer.address, body.features.first().properties!!.layer)
+    }
+
+    @Test
+    fun testSearch() {
+        val res = service.search(address).execute()
+        val body = res.body() ?: fail("Request failed: ${res.errorBody()}")
+
+        assertEquals("Estonia", body.features.first().properties!!.country)
+        assertEquals(PeliasLayer.address, body.features.first().properties!!.layer)
+    }
+
+    @Test
+    fun testSearchStructured() {
+        val res = service.searchStructured(address = address, country = "Estonia").execute()
+        val body = res.body() ?: fail("Request failed: ${res.errorBody()}")
+
+        assertEquals("Estonia", body.features.first().properties!!.country)
+        assertEquals(PeliasLayer.address, body.features.first().properties!!.layer)
+    }
+
+    @Test
+    fun testReverse() {
+        val res = service.reverse(59.444351, 24.750645).execute()
+        val body = res.body() ?: fail("Request failed: ${res.errorBody()}")
+
+        assertEquals("Estonia", body.features.first().properties!!.country)
+        assertEquals(PeliasLayer.address, body.features.first().properties!!.layer)
+    }
+
+    @Test
+    fun testReverseUncommonLayer() {
+        val res = service.reverse(24.750645, 59.444351).execute()
+        val body = res.body() ?: fail("Request failed: ${res.errorBody()}")
+
+        assertEquals(PeliasLayer.marinearea, body.features.first().properties!!.layer)
+    }
+
+    @Test
+    fun testPlace() {
+        val res = service.place(CollectionFormats.CSVParams("openstreetmap:address:way/109867749")).execute()
+        val body = res.body() ?: fail("Request failed: ${res.errorBody()}")
+
+        assertEquals("Estonia", body.features.first().properties!!.country)
+        assertEquals(PeliasLayer.address, body.features.first().properties!!.layer)
+    }
+}
